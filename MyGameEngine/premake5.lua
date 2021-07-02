ccd11c890f76a27811549f7aabc9e3f98ea2ea46
Enes Kızılcin <nazifenes.kizilcin@gmail.com>
2021-07-03 02:05:46 +0300

    premake configurations added
    premake.lua file created with needed configurations, include directories, postbuild commands or etc.. . ConfigureProjects.bat file created which start the premake5.exe for vs2019 and constructs the projects with the configurations writed on premake.lua. For now project can build  as c++ visual studio solution with executing generatedprojects.bat.

---
+++ MyGameEngine/premake5.lua
@@ -0,0 +1,105 @@
+workspace "MyGameEngine"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release",
+        "Dist"
+    }
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "MyGameEngine"
+    location "MyGameEngine"
+    kind "SharedLib"
+    language "C++"
+
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "MyGameEngine/MyGameEngine/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+    
+    defines
+    {
+        "HZ_PLATFORM_WINDOWS",
+        "HZ_BUILD_DLL"
+    }
+
+    postbuildcommands
+    {
+        ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+    }
+    
+    filter "configurations:Debug"
+        defines "MY_ENG_DEBUG"
+        symbols "On"
+
+    filter "configurations:Release"
+        defines "MY_ENG_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "MY_ENG_DIST"
+        optimize "On"
+    
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" ..outputdir.. "/%{prj.name}")
+    objdir ("bin-int/" ..outputdir.. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "MyGameEngine/MyGameEngine/vendor/spdlog/include",
+        "MyGameEngine/src"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On"
+        systemversion "latest"
+    
+    defines
+    {
+        "HZ_PLATFORM_WINDOWS"
+    }
+    links
+    {
+        "MyGameEngine"
+    }
+    
+    filter "configurations:Debug"
+        defines "MY_ENG_DEBUG"
+        symbols "On"
+    
+    filter "configurations:Release"
+        defines "MY_ENG_RELEASE"
+        optimize "On"
+    
+    filter "configurations:Dist"
+        defines "MY_ENG_DIST"
+        optimize "On"
+    
